a4ad5df76613ca40281deb6dfffdfa74fdf95c89
Olivia Lam <livilammister@gmail.com>
2019-10-27 14:51:26 -0500

    repo table is sortable

--- client/components/RepoStats.js
+++ client/components/RepoStats.js
@@ -1,43 +1,39 @@
+import _ from 'lodash'
 import React, {Component} from 'react'
 import {Table} from 'semantic-ui-react'
 
 const octokit = require('@octokit/rest')()
 
-const FAKE_REPOS = [
-  {
-    name: 'angular',
-    forks_count: 0,
-    open_issues_count: 0,
-    watchers_count: 0
-  },
-  {
-    name: 'ember',
-    forks_count: 0,
-    open_issues_count: 0,
-    watchers_count: 0
-  },
-  {
-    name: 'react',
-    forks_count: 0,
-    open_issues_count: 0,
-    watchers_count: 0
-  },
-  {
-    name: 'vue',
-    forks_count: 0,
-    open_issues_count: 0,
-    watchers_count: 0
-  }
-]
-
-const BASE_URL = ''
+const NAME = 'name'
+const FORKS_COUNT = 'forks_count'
+const OPEN_ISSUES_COUNT = 'open_issues_count'
+const WATCHERS_COUNT = 'watchers_count'
 
 export default class RepoStats extends Component {
   constructor() {
     super()
     this.state = {
-      repos: []
+      column: null,
+      data: [],
+      direction: null
     }
+  }
+
+  handleSort = clickedColumn => () => {
+    const {column, data, direction} = this.state
+
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        data: _.sortBy(data, [clickedColumn]),
+        direction: 'ascending'
+      })
+      return
+    }
+    this.setState({
+      data: data.reverse(),
+      direction: direction === 'ascending' ? 'descending' : 'ascending'
+    })
   }
 
   async componentDidMount() {
@@ -46,46 +42,56 @@ export default class RepoStats extends Component {
     const REACT = await octokit.repos.get({owner: 'facebook', repo: 'react'})
     const VUE = await octokit.repos.get({owner: 'vuejs', repo: 'vue'})
 
-    this.setState({repos: [ANGULAR.data, EMBER.data, REACT.data, VUE.data]})
+    this.setState({data: [ANGULAR.data, EMBER.data, REACT.data, VUE.data]})
   }
 
   render() {
+    const {column, data, direction} = this.state
     return (
-      // <div>
-      //     <h1>RepoStats</h1>
-      //     {
-      //         this.state.repos.map(repo => {
-      //             return (
-      //                 <div key={repo.name}>
-      //                     {repo.name}
-      //                     {repo.forks_count}
-      //                     {repo.open_issues_count}
-      //                     {repo.watchers_count}
-      //                 </div>
-      //             )
-      //         })
-      //     }
-      // </div>
       <div>
-        <Table celled>
+        <h1>Framework Stats</h1>
+        <Table celled sortable striped>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell />
-              <Table.HeaderCell>Forks</Table.HeaderCell>
-              <Table.HeaderCell>Open Issues</Table.HeaderCell>
-              <Table.HeaderCell>Watchers</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === NAME ? direction : null}
+                onClick={this.handleSort(NAME)}
+              >
+                Name
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === FORKS_COUNT ? direction : null}
+                onClick={this.handleSort(FORKS_COUNT)}
+              >
+                Forks
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === OPEN_ISSUES_COUNT ? direction : null}
+                onClick={this.handleSort(OPEN_ISSUES_COUNT)}
+              >
+                Open Issues
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === WATCHERS_COUNT ? direction : null}
+                onClick={this.handleSort(WATCHERS_COUNT)}
+              >
+                Watchers
+              </Table.HeaderCell>
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
-            {this.state.repos.map(repo => (
-              <Table.Row>
-                <Table.Cell>{repo.name}</Table.Cell>
-                <Table.Cell>{repo.forks_count}</Table.Cell>
-                <Table.Cell>{repo.open_issues_count}</Table.Cell>
-                <Table.Cell>{repo.watchers_count}</Table.Cell>
-              </Table.Row>
-            ))}
+            {_.map(
+              data,
+              ({name, forks_count, open_issues_count, watchers_count}) => (
+                <Table.Row key={name}>
+                  <Table.Cell>{name}</Table.Cell>
+                  <Table.Cell>{forks_count}</Table.Cell>
+                  <Table.Cell>{open_issues_count}</Table.Cell>
+                  <Table.Cell>{watchers_count}</Table.Cell>
+                </Table.Row>
+              )
+            )}
           </Table.Body>
         </Table>
       </div>
--- client/index.js
+++ client/index.js
@@ -1,12 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Router} from 'react-router-dom'
-import history from './history'
 import App from './app'
 
-ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById('app'))
