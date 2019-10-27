8d50064ae2929d85fcd8676e2d0bbbc7e21125a0
Olivia Lam <livilammister@gmail.com>
2019-10-27 17:44:56 -0500

    feature complete

--- client/components/VoteForm.js
+++ client/components/VoteForm.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, {Component} from 'react'
-import {Button, Form, Input, Select} from 'semantic-ui-react'
+import {Form, Dropdown} from 'formsy-semantic-ui-react'
+import {Label} from 'semantic-ui-react'
 
 const FRAMEWORK_OPTIONS = [
   {text: 'Angular', value: 'angular'},
@@ -23,26 +24,65 @@ export default class VoteForm extends Component {
   handleChange = (e, {name, value}) => this.setState({[name]: value})
 
   handleSubmit = async e => {
-    e.preventDefault()
     const {email, vote} = this.state
     await axios.put('/api/user', {email, vote})
   }
 
   render() {
     const {email, vote} = this.state
+    const errorLabel = <Label color="red" pointing />
     return (
       <div>
         <h1>Submit Your Vote</h1>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onValidSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              required
+              name="email"
+              placeholder="Enter Your Email"
+              validations="isEmail"
+              validationErrors={{
+                isEmail: 'This is not a valid email',
+                isDefaultRequiredValue: 'Email is Required'
+              }}
+              value={email}
+              onChange={this.handleChange}
+            />
+            <Dropdown
+              name="vote"
+              placeholder="Select Framework"
+              label="Framework"
+              required
+              search
+              selection
+              validationErrors={{
+                isDefaultRequiredValue: 'You need to select a product'
+              }}
+              errorLabel={errorLabel}
+              options={FRAMEWORK_OPTIONS}
+              value={vote}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Button content="Submit" color="green" />
+        </Form>
+
+        {/* <Form onSubmit={this.handleSubmit}>
           <Form.Group>
             <Input
               placeholder="Enter Your Email"
               name="email"
+              label="Email"
+              validations="isEmail"
+              validationErrors={{
+                isEmail: 'This is not a valid email',
+                isDefaultRequiredValue: 'Email is Required',
+              }}
+              errorLabel={ errorLabel }
               value={email}
               onChange={this.handleChange}
             />
             <Select
-              placeholder="Select Your Framework"
               name="vote"
               value={vote}
               options={FRAMEWORK_OPTIONS}
@@ -50,7 +90,7 @@ export default class VoteForm extends Component {
             />
             <Button content="Submit" type="submit" />
           </Form.Group>
-        </Form>
+        </Form> */}
       </div>
     )
   }
